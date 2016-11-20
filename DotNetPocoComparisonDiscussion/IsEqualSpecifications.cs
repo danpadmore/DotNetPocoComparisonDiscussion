262fd5638bce6b19144ddf8137a4694d24845915
Dan Padmore <dan.padmore@zna.be>
2016-11-20 21:06:20 +0100

    Add source code from discussion

---
+++ DotNetPocoComparisonDiscussion/IsEqualSpecifications.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DotNetPocoComparisonDiscussion
+{
+    [TestClass]
+    public class IsEqualSpecifications
+    {
+        protected PlainOldClrOjbect Original { get; private set; }
+        protected PlainOldClrOjbect New { get; private set; }
+
+        [TestInitialize]
+        public void Arrange()
+        {
+            Original = new PlainOldClrOjbect { Id = Guid.NewGuid(), Name = "test-name" };
+            New = new PlainOldClrOjbect { Id = Original.Id, Name = Original.Name, NewlyAddedProperty = "new-property" };
+        }
+
+        [TestMethod]
+        public void GivenIsEqualByManualComparison_ThenResultShouldBeFalse()
+        {
+            var result = Original.IsEqualByManualComparison(New);
+
+            Assert.IsFalse(result, "Fails as expected because developer 'forgot' to take newly added property into account");
+        }
+
+        [TestMethod]
+        public void GivenIsEqualBySerializedStringComparison_ThenResultShouldBeFalse()
+        {
+            var result = Original.IsEqualBySerializedStringComparison(New);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void GivenIsEqualByReflection_ThenResultShouldBeFalse()
+        {
+            var result = Original.IsEqualByReflection(New);
+
+            Assert.IsFalse(result);
+        }
+    }
+}
---
+++ DotNetPocoComparisonDiscussion/PlainOldClrOjbect.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace DotNetPocoComparisonDiscussion
+{
+    public class PlainOldClrOjbect
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        /// <summary>
+        /// A property that was added in a newer versino
+        /// </summary>
+        public string NewlyAddedProperty { get; set; }
+
+        /// <summary>
+        /// This method carries risks of developer forgetting to extend it with new properties
+        /// </summary>
+        public bool IsEqualByManualComparison(PlainOldClrOjbect other)
+        {
+            if (other == null)
+                return false;
+
+            return other.Id == Id
+                && other.Name == Name;
+            // Developer then forgets to add NewlyAddedProperty to comparison
+        }
+
+        /// <summary>
+        /// This method incurs performance penalty for serialization
+        /// </summary>
+        public bool IsEqualBySerializedStringComparison(PlainOldClrOjbect other)
+        {
+            var serializer = new DataContractJsonSerializer(GetType());
+
+            using (var sourceStream = new MemoryStream())
+            using (var otherStream = new MemoryStream())
+            {
+                serializer.WriteObject(sourceStream, this);
+                var sourceSerialized = Encoding.Default.GetString(sourceStream.ToArray());
+
+                serializer.WriteObject(otherStream, other);
+                var otherSerialized = Encoding.Default.GetString(otherStream.ToArray());
+
+                return (sourceSerialized == otherSerialized);
+            }
+        }
+
+        /// <summary>
+        /// This method incurs performance penalty for reflection
+        /// </summary>
+        public bool IsEqualByReflection(PlainOldClrOjbect other)
+        {
+            var properties = GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            foreach (var property in properties)
+            {
+                var source = property.GetValue(this);
+                var otherValue = property.GetValue(other);
+
+                if (source == null && otherValue != null)
+                    return false;
+
+                if (source != null && otherValue == null)
+                    return false;
+
+                if (!source.Equals(otherValue))
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
